8ea015a880a8575d3acab1c71826fd57469339e3
iyin0 <p2iyinoluwa@gmail.com>
2024-10-17 11:34:30 +0100

    lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,38 @@ module.exports = {
   rules: {
     'no-console': ['warn', { allow: ['warn', 'error'] }],
     'no-unused-vars': 'error',
-    'import/no-unresolved': 'error',
-    'prettier/prettier': 'error',
+    'import/no-unresolved': ['error', { ignore: ['^@'] }],
     'import/prefer-default-export': 'off',
   },
+  overrides: [
+    {
+      files: ['components/ui/**/*.tsx'],
+      rules: {
+        'react/jsx-props-no-spreading': 'off',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      rules: {
+        '@next/next/no-html-link-for-pages': 'off',
+        'react/react-in-jsx-scope': 'off',
+        '@nx/enforce-module-boundaries': 'off',
+        'import/prefer-default-export': 'off',
+        'react/require-default-props': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/prop-types': 'off',
+        'react/no-unescaped-entities': 'off',
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {},
+    },
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {},
+    },
+  ],
 };
